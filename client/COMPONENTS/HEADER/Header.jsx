1de2ad86e8a5069d3bbc549de9e0cb178bafe937
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-09-14 12:35:52 +0300

    add a logo image in header component

--- client/COMPONENTS/HEADER/Header.jsx
+++ client/COMPONENTS/HEADER/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import './Header.css'
+import { Link } from 'react-router-dom'
+import logo from '../../src/assets/image1.png'
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+        <img src={logo} alt={logo} className='logo' />
+        <Link to={'/'}>Home</Link>
+        <Link to={'/Footer'}>Footer</Link>
+
+    </div>
   )
 }
 
