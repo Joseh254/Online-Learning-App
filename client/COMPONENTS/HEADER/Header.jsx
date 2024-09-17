c0b4bf213c4a76ffcc81e183ace3864839343e4e
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-09-17 12:02:54 +0300

    add a search icon

--- client/COMPONENTS/HEADER/Header.jsx
+++ client/COMPONENTS/HEADER/Header.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
-import './Header.css'
-import { Link } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
-import logo from '../../src/assets/image1.png'
-function Header() {
-const navigate =useNavigate()
+import React from 'react';
+import './Header.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaHome } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
+import logo from '../../src/assets/image1.png';
 
-  function handleNavigateLogin(){
-navigate('/Login')
+function Header() {
+  const navigate = useNavigate();
+
+  function handleNavigateLogin() {
+    navigate('/Login');
   }
+
+  function handleNavigateSignup() {
+    navigate('/Signup');
+  }
+
   return (
     <div className='header'>
-        <img src={logo} alt={logo} className='logo' />
-        <input type="text" placeholder='Search for anything' className='headerSearchInput' />
-        <Link to={'/Enrollment'} style={{textDecoration:'none',color:'#000',fontSize:'1.9rem'}}>Enrollment</Link>
-        <Link to={'/TeachWiithUs'} style={{textDecoration:'none',color:'#000',fontSize:'1.9rem'}}>Teach with Us</Link>
-        <button style={{padding:'1rem', backgroundColor:'#fff'}}onClick={handleNavigateLogin}>Log in</button>
-        <button style={{padding:'1rem', backgroundColor:"#000", color:"#fff"}}>Sign up</button>
-
+      <img src={logo} alt='Logo' className='logo' />
+      <div className='searchWrapper'>
+        <FaSearch className='searchIcon' />
+        <input
+          type="text"
+          placeholder='Search for anything'
+          className='headerSearchInput'
+        />
+      </div>
+      <Link to='/Enrollment' className='navLink'>Enrollment</Link>
+      <Link to='/TeachWiithUs' className='navLink'>Teach with Us</Link>
+      <button className='loginButton' onClick={handleNavigateLogin}>Log in</button>
+      <button className='signupButton' onClick={handleNavigateSignup}>Sign up</button>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
